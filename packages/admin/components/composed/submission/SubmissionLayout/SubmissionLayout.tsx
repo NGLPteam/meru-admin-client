788f83379c5be18b62743ed5a16ccea8a9b954e0
1aurend <imlcdavidson@gmail.com>
2026-02-24 11:30:51 -0800

    [C] Scaffold review submission drawer

--- packages/admin/components/composed/submission/SubmissionLayout/SubmissionLayout.tsx
+++ packages/admin/components/composed/submission/SubmissionLayout/SubmissionLayout.tsx
@@ -1,5 +1,7 @@
+import { useTranslation } from "react-i18next";
 import { graphql } from "react-relay";
 import { useChildRouteLinks, useMaybeFragment, useRouteSlug } from "hooks";
+import { ButtonControlGroup, ButtonControlDrawer } from "components/atomic";
 import { PageHeader, BackToAll } from "components/layout";
 import HtmlHead from "components/global/HtmlHead";
 import { MOCK_SUBMISSIONS } from "components/composed/submission/SubmissionList/mockData";
@@ -18,6 +20,7 @@ export default function SubmissionLayout({
   parentRoute?: string;
   detailRoute?: string;
 }) {
+  const { t } = useTranslation();
   const routeSlug = useRouteSlug() || undefined;
   const slug = slugProp || routeSlug;
   const tabRoutes = useChildRouteLinks(detailRoute, { slug });
@@ -27,12 +30,29 @@ export default function SubmissionLayout({
   const title =
     item?.title || MOCK_SUBMISSIONS.find((s) => s.slug === slug)?.title;
 
+  const buttons = parentRoute === "submissions" && (
+    <ButtonControlGroup toggleLabel={t("options")} menuLabel={t("options")}>
+      <ButtonControlDrawer
+        drawer="reviewSubmission"
+        drawerQuery={{ drawerSlug: slug || "" }}
+        icon="edit"
+      >
+        {t("actions.review_submission")}
+      </ButtonControlDrawer>
+    </ButtonControlGroup>
+  );
+
   return (
     <>
       <HtmlHead title={title} />
       <section>
         <BackToAll route={parentRoute} />
-        <PageHeader title={title} tabRoutes={tabRoutes} tabLinksOnly />
+        <PageHeader
+          title={title}
+          tabRoutes={tabRoutes}
+          tabLinksOnly
+          buttons={buttons || undefined}
+        />
         {children}
       </section>
     </>
