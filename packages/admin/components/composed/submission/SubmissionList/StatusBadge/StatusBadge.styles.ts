4b00a49b98be07b54e6475ede861b84127ea26c2
1aurend <imlcdavidson@gmail.com>
2026-05-12 16:15:55 -0700

    [B] Ensure status badge fits column

--- packages/admin/components/composed/submission/SubmissionList/StatusBadge/StatusBadge.styles.ts
+++ packages/admin/components/composed/submission/SubmissionList/StatusBadge/StatusBadge.styles.ts
@@ -6,7 +6,6 @@ export const Badge = styled.span<{ $bg: string; $color: string }>`
   border-radius: var(--border-radius-xlg);
   font-size: var(--font-size-label-sm);
   font-weight: var(--font-weight-medium);
-  white-space: nowrap;
   text-align: center;
   background-color: ${({ $bg }) => $bg};
   color: ${({ $color }) => $color};
